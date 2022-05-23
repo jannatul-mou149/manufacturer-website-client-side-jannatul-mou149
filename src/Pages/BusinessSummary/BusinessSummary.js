a0aa497e28e582eec8752a3c6b89019f9dfa3747
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-05-23 19:00:22 +0600

    design update

--- src/Pages/BusinessSummary/BusinessSummary.js
+++ src/Pages/BusinessSummary/BusinessSummary.js
@@ -9,6 +9,28 @@ const BusinessSummary = () => {
                     <h1>46,000</h1>
                     <p className='font-bold mt-3'>Deliveries All Over The World</p>
                 </div>
+                <div className='numbers'>
+                    <div className='number'>
+                        <i class="fa-solid fa-flag"></i>
+                        <h6>56+</h6>
+                        <h5>Total Countries</h5>
+                    </div>
+                    <div className='number'>
+                        <i class="fa-solid fa-truck-fast"></i>
+                        <h6>62k+</h6>
+                        <h5>Success Deliveries</h5>
+                    </div>
+                    <div className='number'>
+                        <i class="fa-solid fa-people-group"></i>
+                        <h6>55k+</h6>
+                        <h5>Happy Clients</h5>
+                    </div>
+                    <div className='number'>
+                        <i class="fa-brands fa-gratipay"></i>
+                        <h6>33k+</h6>
+                        <h5>Total Feedback</h5>
+                    </div>
+                </div>
                 <div className="brands">
                     <div className="img">
                         <img src='https://brilliantmaps.com/wp-content/uploads/2015/01/Flag_of_Turkey.png' alt="" />
@@ -32,6 +54,7 @@ const BusinessSummary = () => {
                         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Flag_of_Malaysia.svg/1200px-Flag_of_Malaysia.svg.png' alt="" />
                     </div>
                 </div>
+
             </div>
             <img className='img-fluid' id="map" src={map} alt="" />
         </div>
