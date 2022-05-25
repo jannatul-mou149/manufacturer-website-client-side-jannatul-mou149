f7fb3629770b28a53b8acde5986bb652af6825a5
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-05-25 18:06:32 +0600

    hero section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
-// import bannerImg from '../../Assests/images/bannerImg.jpg';
+import bannerImg from '../../Assests/images/bannerImg.jpg';
 const Banner = () => {
     return (
         <div class="hero min-h-screen" style={{
-            background: `url(https://i.redd.it/2ln5zbm1gvm01.jpg)`,
+            background: `url(https://www.hdwallpapers.in/download/black_laptop_computer_on_brown_wooden_table_4k_5k_hd_cool-1920x1080.jpg)`,
+            backgroundSize: 'cover'
 
         }}>
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src='https://i.ytimg.com/vi/kPB-GJkcopE/maxresdefault.jpg' className="max-w-sm rounded-lg shadow-2xl" alt='' />
+            <div className="hero-content">
+
                 <div>
-                    <h1 className="text-5xl font-bold text-white">Welcome to Dream PC Build</h1>
+                    <h1 className="text-5xl font-bold text-white">Dream PC Build</h1>
                     <p className="py-6 text-white">Dream PC Build Ltd is the largest computer shop and IT Solution Provider in Bangladesh. We provide you with Genuine IT related products like Desktop PCs, Laptops, servers, workstation and many other products to help you achieve your goals..</p>
                     <button class="btn btn-primary">Get Started</button>
                 </div>
