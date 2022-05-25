e07803b370a0dc34d3da60256e5b3f1305376826
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-05-25 18:38:01 +0600

    services edited

--- src/Pages/Home/OurServices.js
+++ src/Pages/Home/OurServices.js
@@ -8,38 +8,38 @@ const OurServices = () => {
                 <div class="services">
                     <div class="service service1">
                         <div class="icon">
-                            <i class="fa-solid fa-robot"></i>
+                            <i class="fa-brands fa-searchengin"></i>
                         </div>
                         <div class="title">
-                            <h3>Lorem, ipsum dolor.</h3>
+                            <h3>Free Diagnostics</h3>
                         </div>
 
                         <div class="des">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint commodi sequi error in consectetur sapiente.</p>
+                            <p>Diagnosis of PC warning light and fault codes with our OBD2 scanner equipment. Book your diagnostic fault code check</p>
                         </div>
                     </div>
                     <div class="service service2">
                         <div class="icon">
-                            <i class="fa-solid fa-robot"></i>
+                            <i class="fa-solid fa-screwdriver-wrench"></i>
                         </div>
                         <div class="title">
-                            <h3>Lorem, ipsum dolor.</h3>
+                            <h3>Quick Repair Process</h3>
                         </div>
 
                         <div class="des">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint commodi sequi error in consectetur sapiente.</p>
+                            <p>Our goal is to see you succeed and we help you reach that through our fast and reliable computer repair services.</p>
                         </div>
                     </div>
                     <div class="service service3">
                         <div class="icon">
-                            <i class="fa-solid fa-robot"></i>
+                            <i class="fa-solid fa-house-circle-check"></i>
                         </div>
                         <div class="title">
-                            <h3>Lorem, ipsum dolor.</h3>
+                            <h3>Home Service</h3>
                         </div>
 
                         <div class="des">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint commodi sequi error in consectetur sapiente.</p>
+                            <p>Largest service marketplace and one-stop solution for your home services in Bangladesh. Order any service</p>
                         </div>
                     </div>
                     <div class="service service4">
@@ -47,11 +47,11 @@ const OurServices = () => {
                             <i class="fa-solid fa-robot"></i>
                         </div>
                         <div class="title">
-                            <h3>Lorem, ipsum dolor.</h3>
+                            <h3>Service Warranty</h3>
                         </div>
 
                         <div class="des">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint commodi sequi error in consectetur sapiente.</p>
+                            <p>We guarantee our service, including replacement parts, for 90 days or the remaining term of your warranty coverage</p>
                         </div>
                     </div>
                 </div>
